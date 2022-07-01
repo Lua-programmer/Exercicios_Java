48a8272dfeb89006dc6494f939323c14c7024122
Luana Melissa <luanamelissaprogrammer@gmail.com>
2022-06-30 22:04:44 -0300

    Exercicio 8 - Estrutura Condicional adicional da questão

--- src/EstruturaCondicionalExercicios/Exercicio8.java
+++ src/EstruturaCondicionalExercicios/Exercicio8.java
@@ -10,6 +10,11 @@ De 0.00 a R$ 2000.00 >> Isento
 De R$ 2000.01 até R$ 3000.00 >> 8%
 De R$ 3000.01 até R$ 4500.00 >> 18%
 Acima de R$ 4500.00 >> 28%
+
+Lembre que, se o salário for R$ 3002.00, a taxa que incide é de 8% apenas sobre R$ 1000.00, pois a faixa de
+salário que fica de R$ 0.00 até R$ 2000.00 é isenta de Imposto de Renda. No exemplo fornecido (abaixo), a taxa é
+de 8% sobre R$ 1000.00 + 18% sobre R$ 2.00, o que resulta em R$ 80.36 no total. O valor deve ser impresso com
+duas casas decimais.
 */
 
 import java.io.PrintStream;
